50739a4d473de7d5de0bef4fb6e998262d2f2c95
Regionnor <s1273226606@163.com>
2024-06-16 14:36:37 +0800

    Site updated: 2024-06-16 14:36:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["Apple-ID/","Gravatar/","HEVC-video/","RSS/","Microsoft365-Yunmu/","Trz7YRgAc/","apple-wallpaper/","bing-wallpapers/","hello-world/","minecraft/","intel-aigc/","yuanshen-4.6-keys/"];function toRandomPost(){
+var posts=["Apple-ID/","Gravatar/","HEVC-video/","Microsoft365-Yunmu/","RSS/","hello-world/","Trz7YRgAc/","intel-aigc/","minecraft/","yuanshen-4.6-keys/","apple-wallpaper/","bing-wallpapers/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
